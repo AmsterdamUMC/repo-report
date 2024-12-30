698dec7d4a74bed1698a6ed13409e69a9ebc659a
tpronk <pronkthomas@gmail.com>
2024-12-30 16:22:54 +0100

    df_n_repo_summary contains summary stats per repo

--- 02.summarize.py
+++ 02.summarize.py
@@ -7,54 +7,92 @@ with open('data/governance.clean.json') as f:
     governance_json = json.load(f)
 
 # Repos
-repos_df = pd.json_normalize(
+df_collaborators = pd.json_normalize(
     governance_json['repos'],
     'collaborators',
     ['full_name']
 )
-repos_df.to_csv('data/governance.repos.csv', index=False)
+# Number of collaborators per repo
+df_n_repo_user_total = \
+    df_collaborators[['full_name', 'login']]. \
+    groupby('full_name', as_index=False). \
+    count(). \
+    rename(columns={'login': 'user_total'})
+
+
+df_n_repo_user_roles = \
+    df_collaborators[['full_name', 'admin', 'maintain', 'push', 'triage', 'pull']]. \
+    groupby('full_name', as_index=False). \
+    sum(). \
+    rename(columns={'admin': 'user_admin', 'maintain': 'user_maintain', 'push': 'user_push', 'triage': 'user_triage', 'pull': 'user_pull'})
 
 # Members
-members_df = pd.json_normalize(
+df_members = pd.json_normalize(
     governance_json['members']
 )
-repos_df.to_csv('data/governance.members.csv', index=False)
 
 # Outside collaborators
-outside_collaborators_df = pd.json_normalize(
+df_ocs = pd.json_normalize(
     governance_json['outside_collaborators']
 )
-outside_collaborators_df.to_csv('data/governance.outside_collaborators.csv', index=False)
+
+# How many members and outside collaborators are collaborators on each repo?
+df_n_repo_members = df_members. \
+    merge(df_collaborators, how='inner') \
+    [['full_name', 'login']]. \
+    groupby('full_name', as_index=False). \
+    count(). \
+    rename(columns={'login': 'member_total'})
+
+df_n_repo_ocs = df_ocs. \
+    merge(df_collaborators, how='inner') \
+    [['full_name', 'login']]. \
+    groupby('full_name', as_index=False). \
+    count(). \
+    rename(columns={'login': 'oc_total'})   
+
 
 # Teams by members
-teams_members_df = pd.json_normalize(
+df_teams_members = pd.json_normalize(
     governance_json['teams'],
     'members',
     ['name']
 )
-teams_members_df.to_csv('data/governance.teams_members.csv', index=False)
 
 # Teams by repositories
-teams_repos_df = pd.json_normalize(
+df_teams_repos = pd.json_normalize(
     governance_json['teams'],
     'repos',
     ['name']
 )
-teams_repos_df.to_csv('data/governance.teams_repos.csv', index=False)
 
+# Number of teams per repo
+df_n_repo_teams = df_teams_repos \
+    [['full_name', 'name']]. \
+    groupby('full_name', as_index=False). \
+    count(). \
+    rename(columns={'name': 'team_total'})
+
+# Merge all repo summary variables together
+df_n_repo_summary = df_n_repo_user_total. \
+    merge(df_n_repo_user_roles, how='left'). \
+    merge(df_n_repo_members, how='left'). \
+    merge(df_n_repo_ocs, how='left'). \
+    merge(df_n_repo_teams, how='left')
+    
 # Organization roles by teams
-organization_roles_teams_df = pd.json_normalize(
+df_organization_roles_teams = pd.json_normalize(
     governance_json['organization_roles'],
     'teams',
     ['name']    
 )
-organization_roles_teams_df.to_csv('data/governance.organization_roles_teams.csv', index=False)
+df_organization_roles_teams.to_csv('data/governance.organization_roles_teams.csv', index=False)
 
 # Organization roles by users
-organization_roles_users_df = pd.json_normalize(
+df_organization_roles_users = pd.json_normalize(
     governance_json['organization_roles'],
     'users',
     ['name']    
 )
-organization_roles_users_df.to_csv('data/governance.organization_roles_users.csv', index=False)
+df_organization_roles_users.to_csv('data/governance.organization_roles_users.csv', index=False)
 
