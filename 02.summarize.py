251b6d3fc9b1b4a9a90292b21e6d7c526ffc28c2
tpronk <pronkthomas@gmail.com>
2025-05-08 12:19:59 +0200

    Add "escalation levels" via department teams

--- 02.summarize.py
+++ 02.summarize.py
@@ -1,9 +1,10 @@
 # 02.summarize.py
 #
 import json, pandas as pd
+pd.options.display.max_rows = 2000
 
 # *** Summarize the governance data into a set of tables
-with open('data/governance.clean.json') as f:
+with open('data-in/governance.clean.json') as f:
     governance_json = json.load(f)
 
 # Repos
@@ -13,15 +14,15 @@ df_collaborators = pd.json_normalize(
     ['full_name']
 )
 # Number of collaborators per repo
-df_n_repo_user_total = \
-    df_collaborators[['full_name', 'login']]. \
+df_n_repo_user_total = df_collaborators \
+    [['full_name', 'login']]. \
     groupby('full_name', as_index=False). \
     count(). \
     rename(columns={'login': 'user_total'})
 
 
-df_n_repo_user_roles = \
-    df_collaborators[['full_name', 'admin', 'maintain', 'push', 'triage', 'pull']]. \
+df_n_repo_user_roles = df_collaborators \
+    [['full_name', 'admin', 'maintain', 'push', 'triage', 'pull']]. \
     groupby('full_name', as_index=False). \
     sum(). \
     rename(columns={'admin': 'user_admin', 'maintain': 'user_maintain', 'push': 'user_push', 'triage': 'user_triage', 'pull': 'user_pull'})
@@ -74,19 +75,18 @@ df_n_repo_teams = df_teams_repos \
     rename(columns={'name': 'team_total'})
 
 # Merge all repo summary variables together
-df_n_repo_summary = df_n_repo_user_total. \
+df_summary = df_n_repo_user_total. \
     merge(df_n_repo_user_roles, how='left'). \
     merge(df_n_repo_members, how='left'). \
     merge(df_n_repo_ocs, how='left'). \
     merge(df_n_repo_teams, how='left')
-    
+
 # Organization roles by teams
 df_organization_roles_teams = pd.json_normalize(
     governance_json['organization_roles'],
     'teams',
     ['name']    
 )
-df_organization_roles_teams.to_csv('data/governance.organization_roles_teams.csv', index=False)
 
 # Organization roles by users
 df_organization_roles_users = pd.json_normalize(
@@ -94,5 +94,74 @@ df_organization_roles_users = pd.json_normalize(
     'users',
     ['name']    
 )
-df_organization_roles_users.to_csv('data/governance.organization_roles_users.csv', index=False)
+#df_organization_roles_users.to_csv('data/governance.organization_roles_users.csv', index=False)
 
+# *** Escalation paths: direct admins, department contact (dc), owner
+
+# Escalation level 3. Organization owners
+owners = df_members \
+    .loc[df_members['role'] == 'admin']['login'] \
+    .to_list()
+
+# Escalation level 2. Department contact (associated with repo or repo admins)
+# df_department_contacts has one row per department
+df_department_contacts = pd \
+    .read_csv('data-in/department-contacts.tsv', sep='\t', header=0) \
+    .rename(columns={'GitHub Username': 'dc_login', 'Department': 'name'}) \
+    [['dc_login', 'name']] \
+    .groupby('name', as_index=False) \
+    .agg({'dc_login': ', '.join})
+
+# Escalation level 1. A valid repo admins: a direct admin of a repo who is an organization member, 
+# but not an organization owner
+df_valid_repo_admins = df_members \
+    .loc[df_members['role'] == 'member'] \
+    .merge(df_collaborators.loc[df_collaborators['admin']]) \
+    [['login', 'full_name']]
+
+# Escalation level 2a. Department teams that the valid repo admins are a member of
+df_joined_valid_repo_admin_departments = df_valid_repo_admins \
+    .merge(df_teams_members) \
+    .merge(df_department_contacts) \
+    .groupby(['full_name'], as_index=False) \
+    .count() \
+    [['full_name']]
+
+df_joined_valid_repo_admin_departments['departments'] =  df_valid_repo_admins \
+    .merge(df_teams_members) \
+    .merge(df_department_contacts) \
+    .groupby(['full_name'], as_index=False) \
+    .agg({'name': 'unique'}) \
+    .agg({'name': ', '.join}) \
+    .rename(columns={'name': 'departments'}) 
+
+df_joined_valid_repo_admins = df_valid_repo_admins \
+    .groupby('full_name', as_index=False) \
+    .agg({'login': ', '.join}) \
+    .rename(columns={'login': 'admins'}) 
+
+# Add escalation paths to summary
+df_summary = df_summary \
+    .merge(df_joined_valid_repo_admins, how='left') \
+    .merge(df_joined_valid_repo_admin_departments, how='left')
+
+df_summary.to_csv('data-out/df_summary.csv', index=False)
+
+
+# Escalation level 2b. Teams with admin rights on a repo. Only used by one department so far; not processed further.
+# df_teams_repos \
+#     .loc[df_teams_repos['admin']]
+
+
+
+    # .agg({'name': ', '.join})
+    # ['name'].agg(['unique']) \
+    # .agg({'unique': ', '.join})
+    # agg({'login': ',u '.join})
+#     [['login', 'name']] \
+
+# . \unique
+#     groupby('full_name', as_index=False). \
+#     agg({'login': ', '.join})
+    #apply(','.join)
+    # rename(columns={'login': 'member_total'})
